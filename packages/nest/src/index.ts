9182b43b5c91698f090853bce49f1b9b08ae7e26
Tristan Chin <tristan.chin@chintristan.io>
2023-04-10 01:11:55 -0400

    Init Nest

--- packages/nest/src/index.ts
+++ packages/nest/src/index.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module.js";
 
-async function bootstrap() {
+const bootstrap = async () => {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
-}
+};
+
 bootstrap();
