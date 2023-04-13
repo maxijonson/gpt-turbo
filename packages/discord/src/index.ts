4c66e351ee7054c396dbb18cd5161ec518dda016
Tristan Chin <tristan.chin@chintristan.io>
2023-04-13 01:46:28 -0400

    [discord] Simplified index file

--- packages/discord/src/index.ts
+++ packages/discord/src/index.ts
@@ -1,5 +1,4 @@
 import { GatewayIntentBits } from "discord.js";
 import GPTTurboClient from "./classes/GPTTurboClient.js";
 
-const client = new GPTTurboClient({ intents: [GatewayIntentBits.Guilds] });
-await client.init();
+new GPTTurboClient({ intents: [GatewayIntentBits.Guilds] }).init();
