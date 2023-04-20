31b8b560cfa8271d08e644ce6355f1ac8bc3ccc0
Tristan Chin <tristan.chin@chintristan.io>
2023-04-20 14:25:00 -0400

    Handle Conversation exceptions

--- packages/discord/src/message-handlers/NewConverversationHandler.ts
+++ packages/discord/src/message-handlers/NewConverversationHandler.ts
@@ -6,7 +6,11 @@ import {
     italic,
 } from "discord.js";
 import MessageHandler from "./MessageHandler.js";
-import { Conversation } from "gpt-turbo";
+import {
+    Conversation,
+    MessageRoleException,
+    ModerationException,
+} from "gpt-turbo";
 import getCleanContent from "../utils/getCleanContent.js";
 import getConversationConfig from "../utils/getConversationConfig.js";
 import EmptyPromptHandler from "./EmptyPromptHandler.js";
@@ -27,19 +31,32 @@ export default class NewConversationHandler extends MessageHandler {
 
     protected async handle(message: Message<boolean>): Promise<void> {
         const prompt = await getCleanContent(message);
-        const promptMessagePromise = message.reply(italic("Thinking..."));
+        const messageReplyPromise = message.reply(italic("Thinking..."));
         try {
             const conversation = new Conversation(getConversationConfig());
-            const [promptMessage, { content }] = await Promise.all([
-                promptMessagePromise,
+            const [messageReply, { content }] = await Promise.all([
+                messageReplyPromise,
                 conversation.prompt(prompt),
             ]);
-            await promptMessage.edit(content);
+            await messageReply.edit(content);
         } catch (error) {
-            console.error(error);
-            (await promptMessagePromise).edit(
-                "There was an error while generating a response."
-            );
+            const messageReply = await messageReplyPromise;
+            if (error instanceof ModerationException) {
+                const flags = error.flags.join(", ");
+                await messageReply.edit(
+                    `Your message (or the response) was flagged for ${flags}.`
+                );
+            } else if (error instanceof MessageRoleException) {
+                // Since this is a new conversation, this should never happen
+                await messageReply.edit(
+                    "There was an issue with the order of the messages. This can happen when two replies in a row are sent by a user/assistant."
+                );
+            } else {
+                console.error(error);
+                await messageReply.edit(
+                    "There was an error while generating a response."
+                );
+            }
         }
     }
 }
