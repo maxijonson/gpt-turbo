973406c40c12aea6576fe549cb0090d209d6094c
Tristan Chin <tristan.chin@chintristan.io>
2023-07-18 17:34:38 -0400

    [web] Update changelog date

--- packages/web/src/changelog/v4-5-0.ts
+++ packages/web/src/changelog/v4-5-0.ts
@@ -3,7 +3,7 @@ import { CHANGELOG_SECTION } from "../config/constants";
 
 const v4_5_0: ChangelogEntry = {
     version: "4.5.0 - Conversation Edit, Duplicate and Export",
-    date: new Date("july 16 2023"),
+    date: new Date("july 18 2023"),
     sections: [
         {
             label: CHANGELOG_SECTION.FEATURES,
