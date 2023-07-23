6bb00f9fbb2e087fc58a4610c38a36535ad7bee2
Tristan Chin <tristan.chin@chintristan.io>
2023-07-23 01:30:07 -0400

    [lib] Convert ConversationOptions to interface

--- packages/lib/src/utils/types/conversation.types.ts
+++ packages/lib/src/utils/types/conversation.types.ts
@@ -1,11 +1,11 @@
 import { ConversationPlugin } from "../../classes/ConversationPlugin.js";
 import { ConversationModel } from "../../index.js";
 
-export type ConversationOptions = ConversationModel & {
+export interface ConversationOptions extends ConversationModel {
     /**
      * Plugins to be used in the conversation.
      *
      * Note that the order of the plugins is important. Each plugin are called in the order they are defined.
      */
     plugins?: ConversationPlugin[];
-};
+}
