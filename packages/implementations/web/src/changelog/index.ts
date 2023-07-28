4ba2ac9e566b0d4d668db3119c0a62f891fa4940
Tristan Chin <tristan.chin@chintristan.io>
2023-07-28 18:15:26 -0400

    [web] Add v5 changelog entry

--- packages/implementations/web/src/changelog/index.ts
+++ packages/implementations/web/src/changelog/index.ts
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import v4_4_0 from "./v4-4-0";
 import v4_4_1 from "./v4-4-1";
 import v4_5_0 from "./v4-5-0";
+import v5_0_0 from "./v5-0-0";
 
 export type ChangelogEntrySection = {
     label: ReactNode;
@@ -17,4 +18,4 @@ export type ChangelogEntry = {
 };
 
 // First entry is the latest version
-export const changelog = [v4_5_0, v4_4_1, v4_4_0];
+export const changelog = [v5_0_0, v4_5_0, v4_4_1, v4_4_0];
