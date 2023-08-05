cae59dc4342cb8d627ca31b48db482b4cbeb96a4
Tristan Chin <tristan.chin@chintristan.io>
2023-08-05 12:59:16 -0400

    [docs] Fix header displacement when spotlight is opened

--- packages/docs/src/app/components/AppHeader/AppHeader.tsx
+++ packages/docs/src/app/components/AppHeader/AppHeader.tsx
@@ -1,20 +1,23 @@
 "use client";
 
-import { Group, Burger, AppShell } from "@mantine/core";
+import { Group, Burger, AppShell, RemoveScroll } from "@mantine/core";
 import AppHeaderLogo from "./AppHeaderLogo/AppHeaderLogo";
 import AppHeaderSearch from "./AppHeaderSearch/AppHeaderSearch";
 import AppHeaderButtons from "./AppHeaderButtons/AppHeaderButtons";
 import { useAppStore } from "@store";
 import { toggleMobileNavbar } from "@store/actions/navbar/toggleMobileNavbar";
-
-export const APPHEADER_HEIGHT = 60;
+import { APPHEADER_HEIGHT } from "@config/constants";
 
 const AppHeader = () => {
     const mobileNavbarOpened = useAppStore((s) => s.navbar.mobileNavbarOpened);
 
     return (
         <>
-            <AppShell.Header h={APPHEADER_HEIGHT} visibleFrom="sm">
+            <AppShell.Header
+                h={APPHEADER_HEIGHT}
+                visibleFrom="sm"
+                className={RemoveScroll.classNames.fullWidth}
+            >
                 <Group
                     justify="space-between"
                     h="100%"
