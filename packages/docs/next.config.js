13b47bb476853d603b56e58ecba1c6a29c27b7f4
Tristan Chin <tristan.chin@chintristan.io>
2023-08-05 14:33:30 -0400

    [docs] Setup MDX

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,10 +1,16 @@
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+
 const withVanillaExtract = createVanillaExtractPlugin();
+
+const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    experimental: {
+        mdxRs: true,
+    },
 };
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = withVanillaExtract(withMDX(nextConfig));
