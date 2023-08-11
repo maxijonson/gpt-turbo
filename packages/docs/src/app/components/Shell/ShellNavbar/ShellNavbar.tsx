660dd0d5b193f439b9f37a47592310f38c9e18bd
Tristan Chin <tristan.chin@chintristan.io>
2023-08-11 15:45:52 -0400

    [docs] Add spacing between navbar groups

--- packages/docs/src/app/components/Shell/ShellNavbar/ShellNavbar.tsx
+++ packages/docs/src/app/components/Shell/ShellNavbar/ShellNavbar.tsx
@@ -1,4 +1,12 @@
-import { AppShell, Box, Tabs, TabsList, TabsTab, Text } from "@mantine/core";
+import {
+    AppShell,
+    Box,
+    Stack,
+    Tabs,
+    TabsList,
+    TabsTab,
+    Text,
+} from "@mantine/core";
 import * as classes from "./ShellNavbar.css";
 import useDocs from "@contexts/hooks/useDocs";
 import { usePathname, useRouter } from "next/navigation";
@@ -11,46 +19,48 @@ const ShellNavbar = () => {
 
     return (
         <AppShell.Navbar className={classes.root} p="md">
-            {docGroupIndexes.map((group) => {
-                const docs = sortDocs(
-                    Object.values(groupedDocs[group.slugGroup])
-                );
+            <Stack>
+                {docGroupIndexes.map((group) => {
+                    const docs = sortDocs(
+                        Object.values(groupedDocs[group.slugGroup])
+                    );
 
-                return (
-                    <Box key={group.slug}>
-                        <Text
-                            className={classes.groupTitle}
-                            fw="bold"
-                            fz="xs"
-                            mb={4}
-                        >
-                            {group.title}
-                        </Text>
-                        <Tabs
-                            orientation="vertical"
-                            placement="right"
-                            value={pathname}
-                        >
-                            <TabsList w="100%">
-                                {docs.map((doc) => (
-                                    <TabsTab
-                                        key={doc.slug}
-                                        value={`/docs/${doc.slug}`}
-                                        onClick={() =>
-                                            router.push(`/docs/${doc.slug}`)
-                                        }
-                                        classNames={{
-                                            tab: classes.tab,
-                                        }}
-                                    >
-                                        {doc.title}
-                                    </TabsTab>
-                                ))}
-                            </TabsList>
-                        </Tabs>
-                    </Box>
-                );
-            })}
+                    return (
+                        <Box key={group.slug}>
+                            <Text
+                                className={classes.groupTitle}
+                                fw="bold"
+                                fz="xs"
+                                mb={4}
+                            >
+                                {group.title}
+                            </Text>
+                            <Tabs
+                                orientation="vertical"
+                                placement="right"
+                                value={pathname}
+                            >
+                                <TabsList w="100%">
+                                    {docs.map((doc) => (
+                                        <TabsTab
+                                            key={doc.slug}
+                                            value={`/docs/${doc.slug}`}
+                                            onClick={() =>
+                                                router.push(`/docs/${doc.slug}`)
+                                            }
+                                            classNames={{
+                                                tab: classes.tab,
+                                            }}
+                                        >
+                                            {doc.title}
+                                        </TabsTab>
+                                    ))}
+                                </TabsList>
+                            </Tabs>
+                        </Box>
+                    );
+                })}
+            </Stack>
         </AppShell.Navbar>
     );
 };
