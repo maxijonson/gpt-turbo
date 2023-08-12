197cb964fabf131ffdddfe65c7315b0a121f7cc8
Tristan Chin <tristan.chin@chintristan.io>
2023-08-11 23:06:49 -0400

    [docs] Scroll navbar

--- packages/docs/src/app/components/Shell/ShellNavbar/ShellNavbar.tsx
+++ packages/docs/src/app/components/Shell/ShellNavbar/ShellNavbar.tsx
@@ -2,16 +2,17 @@ import {
     Anchor,
     AppShell,
     Box,
+    ScrollArea,
     Stack,
     Tabs,
     TabsList,
     TabsTab,
 } from "@mantine/core";
-import * as classes from "./ShellNavbar.css";
 import useDocs from "@contexts/hooks/useDocs";
 import { usePathname, useRouter } from "next/navigation";
-import sortDocs from "../../../../utils/sortDocs";
+import sortDocs from "@utils/sortDocs";
 import Link from "next/link";
+import * as classes from "./ShellNavbar.css";
 
 const ShellNavbar = () => {
     const pathname = usePathname();
@@ -19,52 +20,56 @@ const ShellNavbar = () => {
     const { groupedDocs, docGroupIndexes } = useDocs();
 
     return (
-        <AppShell.Navbar className={classes.root} p="md">
-            <Stack>
-                {docGroupIndexes.map((group) => {
-                    const docs = sortDocs(
-                        Object.values(groupedDocs[group.slugGroup])
-                    );
+        <AppShell.Navbar className={classes.root} px="md" py={0}>
+            <ScrollArea type="never" offsetScrollbars={false}>
+                <Stack py="md">
+                    {docGroupIndexes.map((group) => {
+                        const docs = sortDocs(
+                            Object.values(groupedDocs[group.slugGroup])
+                        );
 
-                    return (
-                        <Box key={group.slug}>
-                            <Anchor
-                                component={Link}
-                                href={`/docs/${group.slugGroup}`}
-                                className={classes.groupTitle}
-                                fw="bold"
-                                fz="xs"
-                                c="inherit"
-                                mb={4}
-                            >
-                                {group.title}
-                            </Anchor>
-                            <Tabs
-                                orientation="vertical"
-                                placement="right"
-                                value={pathname}
-                            >
-                                <TabsList w="100%">
-                                    {docs.map((doc) => (
-                                        <TabsTab
-                                            key={doc.slug}
-                                            value={`/docs/${doc.slug}`}
-                                            onClick={() =>
-                                                router.push(`/docs/${doc.slug}`)
-                                            }
-                                            classNames={{
-                                                tab: classes.tab,
-                                            }}
-                                        >
-                                            {doc.title}
-                                        </TabsTab>
-                                    ))}
-                                </TabsList>
-                            </Tabs>
-                        </Box>
-                    );
-                })}
-            </Stack>
+                        return (
+                            <Box key={group.slug}>
+                                <Anchor
+                                    component={Link}
+                                    href={`/docs/${group.slugGroup}`}
+                                    className={classes.groupTitle}
+                                    fw="bold"
+                                    fz="xs"
+                                    c="inherit"
+                                    mb={4}
+                                >
+                                    {group.title}
+                                </Anchor>
+                                <Tabs
+                                    orientation="vertical"
+                                    placement="right"
+                                    value={pathname}
+                                >
+                                    <TabsList w="100%">
+                                        {docs.map((doc) => (
+                                            <TabsTab
+                                                key={doc.slug}
+                                                value={`/docs/${doc.slug}`}
+                                                onClick={() =>
+                                                    router.push(
+                                                        `/docs/${doc.slug}`
+                                                    )
+                                                }
+                                                classNames={{
+                                                    tab: classes.tab,
+                                                }}
+                                            >
+                                                {doc.title}
+                                            </TabsTab>
+                                        ))}
+                                    </TabsList>
+                                </Tabs>
+                            </Box>
+                        );
+                    })}
+                </Stack>
+            </ScrollArea>
         </AppShell.Navbar>
     );
 };
